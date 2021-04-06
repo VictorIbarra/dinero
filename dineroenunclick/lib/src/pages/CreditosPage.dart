d8c9efad58719ba35432cf42e8b167186d7268f4
amaury-tobias <isidro.tobias@prestamofeliz.com.mx>
2021-04-06 18:06:55 -0500

    refactor: `solicita` page move list items to custom widget

--- dineroenunclick/lib/src/pages/CreditosPage.dart
+++ dineroenunclick/lib/src/pages/CreditosPage.dart
@@ -1,10 +1,9 @@
 import 'package:dineroenunclick/src/models/CreditoModel.dart';
 import 'package:dineroenunclick/src/models/UsuarioModel.dart';
 import 'package:dineroenunclick/src/providers/CreditoProvider.dart';
+import 'package:dineroenunclick/src/widgets/SolicitaItem.dart';
 import 'package:flutter/material.dart';
-import 'package:dineroenunclick/src/utilities/metodos.dart';
 import 'package:dineroenunclick/src/utilities/constants.dart';
-import 'package:flutter/services.dart';
 
 class CreditosPage extends StatefulWidget {
   CreditosPage({Key key}) : super(key: key);
@@ -18,43 +17,13 @@ BoxDecoration myBoxDecoration(Color color) {
   );
 }
 
-TextStyle _textStyle(String tipo, Color color) {
-  TextStyle ts = TextStyle();
-  switch (tipo) {
-    case 'hBold':
-      ts = TextStyle(
-        color: color,
-        fontSize: 24.0,
-        fontWeight: FontWeight.w900,
-        fontFamily: 'Montserrat',
-      );
-      break;
-    case 'hLight':
-      ts = TextStyle(
-        color: color,
-        fontSize: 12.0,
-        fontFamily: 'Montserrat',
-      );
-      break;
-    case 'sBold':
-      ts = TextStyle(
-        color: color,
-        fontSize: 12.0,
-        fontWeight: FontWeight.w700,
-        fontFamily: 'Montserrat',
-      );
-      break;
-  }
-  return ts;
-}
-
 class _CreditosPageState extends State<CreditosPage> {
   Widget _loadCreditos(BuildContext context) {
     return FutureBuilder(
       future: CreditoProvider.creditosCliente(Usuario.usr.clienteId),
       builder: (BuildContext context, AsyncSnapshot<List<Credito>> snapshot) {
         if (snapshot.hasData) {
-          if (snapshot.data.isEmpty)
+          if (snapshot.data.isEmpty) {
             return Container(
               alignment: Alignment.center,
               child: Column(
@@ -77,6 +46,7 @@ class _CreditosPageState extends State<CreditosPage> {
                 ],
               ),
             );
+          }
           return _drawVisitanteItems(snapshot.data, context);
         } else {
           return Container(
@@ -86,203 +56,12 @@ class _CreditosPageState extends State<CreditosPage> {
     );
   }
 
-  Widget _itemCredito(BuildContext context, Credito item) {
-    final _screenSize = MediaQuery.of(context).size;
-    final marginTop = 10.0;
-    final marginLeft = 20.0;
-    double _currentSliderValue = 10;
-
-    List<Widget> textosL = new List<Widget>();
-    textosL.add(Container(
-      margin: EdgeInsets.only(left: marginLeft),
-      child: Column(
-        children: [
-          Text('Disponible', style: _textStyle('hBold', pfazul2)),
-          SizedBox(height: 30),
-          Slider(
-            value: _currentSliderValue,
-            min: 0,
-            max: 100,
-            divisions: 30,
-            label: _currentSliderValue.round().toString(),
-            activeColor: pfazul2,
-            onChanged: (double value) {
-              setState(() {
-                _currentSliderValue = value;
-              });
-              print(_currentSliderValue.toString());
-            },
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 15.0),
-            width: MediaQuery.of(context).size.width * .6,
-            child: RaisedButton(
-              elevation: 5.0,
-              onPressed: () {},
-              padding: EdgeInsets.all(15),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)),
-              color: pfVerde2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'en 1 Click',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Montserrat'),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),
-    ));
-
-    textosL.add(Container(
-      margin: EdgeInsets.only(top: marginTop, left: 10.0),
-      width: 360,
-      height: 60.0,
-      decoration: myBoxDecoration(Colors.green),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                'MONTO DISPONIBLE',
-                style: TextStyle(fontSize: 10.0, color: pffgris2),
-              ),
-              Text('       '),
-              Text(
-                'PLAZO',
-                style: TextStyle(fontSize: 10.0, color: pffgris2),
-              ),
-              Text('       '),
-              Text(
-                'PAGO MENUAL',
-                style: TextStyle(fontSize: 10.0, color: pffgris2),
-              ),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-          ),
-          Row(
-            children: [
-              Text('\$${comma(item.disponible?.toString())}',
-                  style: TextStyle(color: pfazul2, fontSize: 18.0)),
-              Row(
-                children: [
-                  Text(' ${comma(item.plazo?.toString())}',
-                      style: TextStyle(color: pfazul2, fontSize: 18.0)),
-                  Text(
-                    ' ${item.frecuencia == 'M' ? 'Meses' : 'Quincenas'}',
-                    style: TextStyle(color: pfazul2, fontSize: 18.0),
-                  ),
-                ],
-              ),
-              Text('\$${comma(item.pago?.toString())}',
-                  style: TextStyle(color: pfazul2, fontSize: 18.0)),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          ),
-        ],
-      ),
-    ));
-
-    textosL.add(Container(
-      margin: EdgeInsets.only(top: marginTop, left: 15.0),
-      width: 360,
-      height: 60.0,
-      decoration: myBoxDecoration(Colors.green),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                '  MONTO ',
-                style: TextStyle(fontSize: 10.0, color: pffgris2),
-              ),
-              Text('       '),
-              Text(
-                '   PLAZO',
-                style: TextStyle(fontSize: 10.0, color: pffgris2),
-              ),
-              Text('       '),
-              Text(
-                '    PAGO MENUAL',
-                style: TextStyle(fontSize: 10.0, color: pffgris2),
-              ),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-          ),
-          Row(
-            children: [
-              Text('\$${comma(item.capital?.toString())}',
-                  style: TextStyle(color: pfazul2, fontSize: 18.0)),
-              Row(
-                children: [
-                  Text(' ${comma(item.plazo?.toString())}',
-                      style: TextStyle(color: pfazul2, fontSize: 18.0)),
-                  Text(
-                    ' ${item.frecuencia == 'M' ? 'Meses' : 'Quincenas'}',
-                    style: TextStyle(color: pfazul2, fontSize: 18.0),
-                  ),
-                ],
-              ),
-              Text('\$${comma(item.pago?.toString())}',
-                  style: TextStyle(
-                    color: pfazul2,
-                    fontSize: 18.0,
-                  )),
-            ],
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          ),
-        ],
-      ),
-    ));
-
-    return GestureDetector(
-      onTap: () async {
-        await Navigator.pushNamed(context, '/promocionDetalle',
-            arguments: item);
-        setState(() => {});
-      },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: marginTop),
-            //tamano de la caja
-            width: _screenSize.width * .95,
-            height: _screenSize.height * .40,
-            decoration: myBoxDecoration(Colors.green),
-            child: Row(
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: textosL,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Widget _drawVisitanteItems(List<Credito> creditos, BuildContext context) {
-    return ListView.separated(
+    return ListView.builder(
       itemCount: creditos.length,
       itemBuilder: (BuildContext context, int index) {
-        return _itemCredito(context, creditos[index]);
+        return SolicitaItem(credito: creditos[index]);
       },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 
---
+++ dineroenunclick/lib/src/widgets/SolicitaItem.dart
@@ -0,0 +1,366 @@
+import 'package:dineroenunclick/src/models/CreditoModel.dart';
+import 'package:dineroenunclick/src/models/UsuarioModel.dart';
+import 'package:dineroenunclick/src/providers/PromocionProvider.dart';
+import 'package:dineroenunclick/src/utilities/constants.dart';
+import 'package:dineroenunclick/src/utilities/debouncer.dart';
+import 'package:dineroenunclick/src/utilities/metodos.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SolicitaItem extends StatefulWidget {
+  final Credito credito;
+  SolicitaItem({this.credito});
+
+  @override
+  _SolicitaItem createState() => _SolicitaItem(credito: credito);
+}
+
+class Title extends StatelessWidget {
+  final data;
+  Title(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      data,
+      style: TextStyle(
+        color: pfazul2,
+        fontSize: 24,
+        fontWeight: FontWeight.w900,
+        fontFamily: 'Montserrat',
+      ),
+    );
+  }
+}
+
+class _SolicitaItem extends State<SolicitaItem> {
+  final GlobalKey<State> _keyLoader = new GlobalKey<State>();
+  final _debouncer = Debouncer();
+  final Credito credito;
+
+  String _pago = '--';
+  double _slider;
+  double _min = 0;
+  int _divisions;
+
+  _SolicitaItem({this.credito});
+
+  @override
+  void initState() {
+    super.initState();
+    _slider = credito.disponible;
+    _min = credito.disponible < 1000
+        ? credito.disponible < 100
+            ? 10
+            : 100
+        : 1000;
+    _divisions = _calculateDivisions(
+        credito.disponible < 1000 ? 1 : 1000, credito.disponible);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.all(15),
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Title('Disponible'),
+              SizedBox(height: 30),
+              Slider.adaptive(
+                value: _slider,
+                min: _min,
+                max: credito.disponible,
+                activeColor: pfazul2,
+                divisions: _divisions,
+                label: _slider.round().toString(),
+                onChanged: (val) {
+                  setState(() {
+                    _slider = val;
+                  });
+                  _debouncer(() {
+                    _handleCalcularPagoMensual();
+                  });
+                },
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: MediaQuery.of(context).size.width * .6,
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: RaisedButton(
+                    elevation: 5,
+                    padding: EdgeInsets.all(15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    color: pfVerde2,
+                    child: Text(
+                      'en 1 Click',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                    onPressed: () {
+                      Dialogs.showConfirmationDialog(
+                        context,
+                        phone: Usuario.usr.telefono,
+                        action: () => _handleSubmit(context),
+                      );
+                    }),
+              ),
+              Container(
+                padding: EdgeInsets.all(4),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'MONTO DISPONIBLE',
+                              style: TextStyle(fontSize: 10, color: pffgris2),
+                            ),
+                            Text(
+                              '\$ ${comma(_slider.toStringAsFixed(0))}',
+                              style: TextStyle(
+                                color: pfazul2,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'PLAZO',
+                              style: TextStyle(fontSize: 10, color: pffgris2),
+                            ),
+                            Text(
+                              '${credito.plazo} ${credito.frecuencia == 'M' ? 'Meses' : 'Quincenas'}',
+                              style: TextStyle(
+                                color: pfazul2,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'PAGO ${credito.frecuencia == 'M' ? 'MENSUAL' : 'QUINCENAL'}',
+                              style: TextStyle(fontSize: 10, color: pffgris2),
+                            ),
+                            Text(
+                              '\$ $_pago',
+                              style: TextStyle(
+                                color: pfazul2,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'MONTO',
+                              style: TextStyle(fontSize: 10, color: pffgris2),
+                            ),
+                            Text(
+                              '\$ ${comma(credito.capital.toStringAsFixed(0))}',
+                              style: TextStyle(
+                                color: pfazul2,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'PLAZO',
+                              style: TextStyle(fontSize: 10, color: pffgris2),
+                            ),
+                            Text(
+                              '${credito.plazo} ${credito.frecuencia == 'M' ? 'Meses' : 'Quincenas'}',
+                              style: TextStyle(
+                                color: pfazul2,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'PAGO ${credito.frecuencia == 'M' ? 'MENSUAL' : 'QUINCENAL'}',
+                              style: TextStyle(fontSize: 10, color: pffgris2),
+                            ),
+                            Text(
+                              '\$ ${credito.pago.toStringAsFixed(0)}',
+                              style: TextStyle(
+                                color: pfazul2,
+                                fontSize: 18,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    _debouncer.cancel();
+    super.dispose();
+  }
+
+  int _calculateDivisions(double montoMin, double montoMax) {
+    int factor = 500;
+    int montoReal = ((montoMax - montoMin).round());
+    int steps = 10;
+    if (montoMax < 100)
+      factor = 10;
+    else if (montoMax < 1000)
+      factor = 100;
+    else if (montoMax < 30000)
+      factor = 100;
+    else
+      factor = 1000;
+
+    steps = (montoReal / factor).round();
+    return steps;
+  }
+
+  Future<void> _handleCalcularPagoMensual() async {
+    final res = await PromocionProvider.calculaMontoMensual(
+      frecuencia: credito.frecuencia,
+      plazo: credito.plazo,
+      taza: credito.tasa,
+      monto: credito.saldoCredito + _slider,
+    );
+
+    if (res.error != 0)
+      setState(() {
+        _pago = "--";
+      });
+    else
+      setState(() {
+        _pago = res.data.pagoErogacion.toStringAsFixed(1);
+      });
+  }
+
+  Future<void> _handleSubmit(BuildContext context) async {
+    Dialogs.showLoadingDialog(context, _keyLoader);
+
+    final valid = await PromocionProvider.subirAplicacionCredito(
+        idSolicitud: credito.idSolicutud,
+        idProducto: credito.idProducto,
+        idSucursal: credito.idSucursal,
+        clabe: credito.clabe,
+        saldoCredito: credito.saldoCredito + _slider,
+        disponible: _slider,
+        phoneNumber: Usuario.usr.telefono);
+    if (valid) {
+      Navigator.of(_keyLoader.currentContext, rootNavigator: true)
+        ..pop()
+        ..popAndPushNamed('/respuestaCredito');
+    } else {
+      Navigator.of(_keyLoader.currentContext, rootNavigator: true)
+        ..pop();
+    }
+  }
+}
+
+class Dialogs {
+  static Future<void> showLoadingDialog(
+      BuildContext context, GlobalKey key) async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return WillPopScope(
+            onWillPop: () async => false,
+            child: SimpleDialog(
+              key: key,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 10),
+                      Text('Por favor espere...'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
+  static Future<void> showConfirmationDialog(BuildContext context,
+      {String phone, Function action}) async {
+    final _formKey = GlobalKey<FormState>();
+
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          elevation: 0,
+          title: Text('Nos contactaremos al siguiente número'),
+          content: Form(
+            key: _formKey,
+            child: TextFormField(
+              textCapitalization: TextCapitalization.characters,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              autofocus: true,
+              validator: (value) {
+                if (value.isEmpty || value.length != 10) return '10 digitos';
+                return null;
+              },
+              decoration: InputDecoration(hintText: '10 digitos'),
+              onChanged: (value) => phone = value,
+              initialValue: phone,
+            ),
+          ),
+          actions: [
+            FlatButton(
+              child: Text('ACEPTAR'),
+              onPressed: () {
+                if (_formKey.currentState.validate()) action();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
